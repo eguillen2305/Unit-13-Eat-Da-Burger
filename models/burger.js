7e605953582d0c1b0744b699790211fae7750e2b
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 13:09:47 -0500

    Updated burger.js ORM function and module export
    ORM method to execute SQL statment and send result . Burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,25 @@
-//
+//Dependencies
+var orm = require('../config/orm.js');
+
+var burger = {
+    selectAll: function (cb) {
+        //ORM Method to execute SQL statement and send result
+		orm.selectAll('burger', function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne('burger', cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(objColVals, condition, cb) {
+		console.log('Model' + objColVals);
+		orm.updateOne('burger', objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
+};
+
+//Exports database function to burger.js
+module.exports = burger;
