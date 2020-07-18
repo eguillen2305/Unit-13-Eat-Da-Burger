7c257de863779de8f7949a64c2ca70075f54c2e8
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 12:30:59 -0500

    Connection server and orm in configure

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,22 @@
-var connection = require("../config/connection.js");
+var connection = require('../config/connection.js');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'password',
+		database: 'burger_db'
+	});
+}
+// Make connection.
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
