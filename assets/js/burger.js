06af458771006d1bc8ba26834db1e0093cbc7346
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 12:22:38 -0500

    function devour

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,1 +1,11 @@
 // burger.js - functions devour, ajaxs, etc needed
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newState = $(this).data("newdevour");
+
+        var newEatingState = {
+            devoured: newState
+        }
+    })
+
