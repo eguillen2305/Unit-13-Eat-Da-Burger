76e3f85dd63418d74d07683a0876544b1486af08
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 22:04:51 -0500

    error still there module not found

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');
 
 var connection;
-//When deployed on heroku
+//Heroku
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
