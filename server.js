d2b0df1b67f8cc705a6add59d4c2a25fc9e9cc74
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 21:40:26 -0500

    node server.js Getting module not found error

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-//Start of server.js
-//NPM INSTALLED
+var express = require('express');
+var app = express();
 
-var express = require("express");
+var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 3000;
+
+var routes = require('./controllers/burger_controller.js');
+
+// Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+//Setting handlebar engines
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+//Start of server
+app.listen(PORT, function() {
+	console.log('Server listening on: http://localhost:' + PORT);
+});
