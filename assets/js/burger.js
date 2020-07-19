1a8d16b29a97280290602ea99946f65ca08beb85
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-18 19:30:38 -0500

    fixed error in burger.js and added background color in css

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,46 +1,40 @@
 // burger.js - functions devour, ajaxs, etc needed
-$(function () {
-    $(".change-devour").on("click", function (event) {
-        var id = $(this).data("id");
-        var newState = $(this).data("newdevour");
+$(function() {
+	$('.change-devour').on('click', function(event) {
+		var id = $(this).data('id');
+		var newState = $(this).data('newdevour');
 
-        var newEatingState = {
-            devoured: newState
-        }
-    })
+		var newEatingState = {
+			devoured: newState
+		};
 
-     $.ajax("/api/burger/" + id, {
-      type: "PUT",
-      data: newEatingState
-    }).then(
-      function() {
-        console.log("changed eat to", newEatingState);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });  
+		$.ajax('/api/burger/' + id, {
+			type: 'PUT',
+			data: newEatingState
+		}).then(function() {
+			console.log('changed eat to', newEatingState);
+			// Reload the page to get the updated list
+			location.reload();
+		});
+	});
 
- $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+	$('.create-form').on('submit', function(event) {
+		// Make sure to preventDefault on a submit event.
+		event.preventDefault();
 
-    var newburger = {
-      cupcake_name: $("#newburger").val().trim(),
-      devoured: 0, //Set to false 
-    };
+		var newburger = {
+			cupcake_name: $('#newburger').val().trim(),
+			devoured: 0 //Set to false
+		};
 
-    // Sends the POST request.
-    $.ajax("/api/burger", {
-      type: "POST",
-      data: newburger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-  
+		// Sends the POST request.
+		$.ajax('/api/burger', {
+			type: 'POST',
+			data: newburger
+		}).then(function() {
+			console.log('created new burger');
+			// Reload the page to get the updated list
+			location.reload();
+		});
+	});
 });
