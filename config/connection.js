9f2b4111ebbfe19f44d2e900f757b698d8d53a82
Eduardo Guillen <eguillen2035@gmail.com>
2020-09-24 18:21:38 -0500

    Fixed issue and now being able to show on local host

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,22 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-  host: 'localhost',
+	var connection = mysql.createConnection({
+		host: 'localhost',
 		port: 3306,
 		user: 'root',
 		password: 'yourRootPassword',
-		database: 'burger_db'
-
-});
+		database: 'burgers_db'
+	});
 }
 
-
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-module.exports = connection;
 
+connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+	if (error) throw error;
+	console.log('The solution is: ', results[0].solution);
+});
+
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,11 @@ DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
--- USE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers (
 	id INT AUTO_INCREMENT,
-    burger_name VARCHAR(255),
+    burger_name VARCHAR(600),
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 )
