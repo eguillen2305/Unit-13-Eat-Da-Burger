4e6795a6d8ec33ad8929c0c74002d847db38f837
Eduardo Guillen <eguillen2035@gmail.com>
2020-09-26 14:03:44 -0500

    Procfile added

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
 	if (error) throw error;
-	console.log('The solution is: ', results[0].solution);
+	console.log('Sucessful connection: ', results[0].solution);
 });
 
 module.exports = connection;
